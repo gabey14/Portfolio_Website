0a25f8bc560368714a3e40cffa879edc4734b1d4
gabey14 <abeygeorge14@gmail.com>
2022-04-29 01:42:40 +0530

    added animation for about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -27,12 +27,12 @@ function About() {
                 paddingBottom: '50px',
               }}
             >
-              <Rotate direction='bottom-left' triggerOnce>
+              <Rotate direction='bottom-left'>
                 <h1 style={{ fontSize: '2.1em', paddingBottom: '20px' }}>
                   Know Who <strong className='purple'>I'M</strong>
                 </h1>
               </Rotate>
-              <Rotate direction='bottom-left' triggerOnce delay={400}>
+              <Rotate direction='bottom-left' delay={400}>
                 <Aboutcard />
               </Rotate>
             </Col>
@@ -41,7 +41,7 @@ function About() {
               style={{ paddingTop: '120px', paddingBottom: '50px' }}
               className='about-img'
             >
-              <Rotate direction='bottom-left' triggerOnce delay={400}>
+              <Rotate direction='bottom-left' delay={400}>
                 <img src={laptopImg} alt='about' className='img-fluid' />
               </Rotate>
             </Col>
