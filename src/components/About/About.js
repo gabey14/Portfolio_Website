698fb2069ac9a6ef12c143d7968360bc9d84ee44
gabey14 <abeygeorge14@gmail.com>
2022-04-29 12:31:49 +0530

    updated about me animation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ import Aboutcard from './AboutCard'
 import laptopImg from '../../Assets/about.png'
 import Toolstack from './Toolstack'
 import { motion } from 'framer-motion'
-import { Rotate, Bounce, Zoom } from 'react-awesome-reveal'
+import { Bounce, Zoom, Flip } from 'react-awesome-reveal'
 function About() {
   return (
     <motion.div
@@ -27,38 +27,38 @@ function About() {
                 paddingBottom: '50px',
               }}
             >
-              <Rotate direction='bottom-left'>
+              <Flip direction='vertical'>
                 <h1 style={{ fontSize: '2.1em', paddingBottom: '20px' }}>
                   Know Who <strong className='purple'>I'M</strong>
                 </h1>
-              </Rotate>
-              <Rotate direction='bottom-left' delay={400}>
+              </Flip>
+              <Flip direction='vertical' delay={300}>
                 <Aboutcard />
-              </Rotate>
+              </Flip>
             </Col>
             <Col
               md={5}
               style={{ paddingTop: '120px', paddingBottom: '50px' }}
               className='about-img'
             >
-              <Rotate direction='bottom-left' delay={400}>
+              <Flip direction='vertical' delay={300}>
                 <img src={laptopImg} alt='about' className='img-fluid' />
-              </Rotate>
+              </Flip>
             </Col>
           </Row>
-          <Bounce cascade fraction={1}>
+          <Bounce cascade fraction={0.7}>
             <h1 className='project-heading'>
               Professional <strong className='purple'>Skillset </strong>
             </h1>
           </Bounce>
           <Techstack />
-          <Bounce cascade>
+          <Bounce cascade fraction={0.7}>
             <h1 className='project-heading'>
               <strong className='purple'>Tools</strong> I use
             </h1>
           </Bounce>
           <Toolstack />
-          <Zoom direction='down' cascade fraction={0}>
+          <Zoom direction='down' cascade fraction={0.7}>
             <Github />
           </Zoom>
         </Container>
