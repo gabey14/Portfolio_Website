43c47981293e89d4a2e93a6450279c5159d8ae66
gabey14 <abeygeorge14@gmail.com>
2022-04-29 14:51:43 +0530

    updated resume animation

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Particle from '../Particle'
-import pdf from '../../Assets/../Assets/Abey-George-Resume.pdf'
+// import pdf from '../../Assets/../Assets/Abey-George-Resume.pdf'
 import {
   AiFillCopy,
   AiOutlineDownload,
@@ -14,6 +14,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import Popup from 'reactjs-popup'
 import './Resume.css'
 import { motion } from 'framer-motion'
+import { Zoom } from 'react-awesome-reveal'
 import { useAlert } from 'react-alert'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
@@ -43,96 +44,103 @@ function ResumeNew() {
     >
       <Container fluid className='resume-section'>
         <Particle />
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button
-            variant='primary'
-            href={pdf}
-            target='_blank'
-            style={{ maxWidth: '250px' }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className='resume'>
-          <Document file={resumeLink} className='d-flex justify-content-center'>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+        <Zoom>
+          <Row style={{ justifyContent: 'center', position: 'relative' }}>
+            <Button
+              variant='primary'
+              href={resumeLink}
+              target='_blank'
+              style={{ maxWidth: '250px' }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV
+            </Button>
+          </Row>
+        </Zoom>
+        <Zoom>
+          <Row className='resume'>
+            <Document
+              file={resumeLink}
+              className='d-flex justify-content-center'
+            >
+              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            </Document>
+          </Row>
+        </Zoom>
 
         <br />
         <br />
         <br />
-
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Popup
-            trigger={
-              <Button
-                variant='primary'
-                // href={pdf}
-                // target='_blank'
-                style={{ maxWidth: '250px' }}
-              >
-                <AiOutlineShareAlt />
-                &nbsp;Share CV
-              </Button>
-            }
-            position='top center'
-            // overlayStyle={}
-            // arrow={false}
-            arrowStyle={{ color: 'purple' }}
-            contentStyle={{
-              background: 'transparent',
-              border: 'none',
-              // padding: '2rem',
-              width: '7rem',
-              height: '5rem',
-              borderRadius: '10px',
-              // boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
-              color: '#fff',
-              // fontSize: '1.5rem',
-              // fontWeight: 'bold',
-              // textAlign: 'center',
-            }}
-            offsetY={5}
-            // closeOnDocumentClick={false}
-          >
-            <div className='popup-icons'>
-              <ul className='footer-icons'>
-                <li className='share-icons'>
-                  <motion.a
-                    className='whatsapp'
-                    href='https://api.whatsapp.com/send?text=https://gabey.me/resume'
-                    data-action='share/whatsapp/share'
-                    style={{ color: 'white', fontSize: '1.2rem' }}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9, x: '-5px', y: '5px' }}
-                  >
-                    <AiOutlineWhatsApp />
-                  </motion.a>
-                </li>
-                <li className='share-icons'>
-                  <motion.button
-                    onClick={() => onClickHandler()}
-                    style={{
-                      color: 'white',
-                      fontSize: '1.2rem',
-                      background: 'transparent',
-                      border: 'none',
-                    }}
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.9, x: '-5px', y: '5px' }}
-                  >
-                    <AiFillCopy />
-                  </motion.button>
-                </li>
-              </ul>
-            </div>
-          </Popup>
-        </Row>
+        <Zoom>
+          <Row style={{ justifyContent: 'center', position: 'relative' }}>
+            <Popup
+              trigger={
+                <Button
+                  variant='primary'
+                  // href={pdf}
+                  // target='_blank'
+                  style={{ maxWidth: '250px' }}
+                >
+                  <AiOutlineShareAlt />
+                  &nbsp;Share CV
+                </Button>
+              }
+              position='top center'
+              // overlayStyle={}
+              // arrow={false}
+              arrowStyle={{ color: 'purple' }}
+              contentStyle={{
+                background: 'transparent',
+                border: 'none',
+                // padding: '2rem',
+                width: '7rem',
+                height: '5rem',
+                borderRadius: '10px',
+                // boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
+                color: '#fff',
+                // fontSize: '1.5rem',
+                // fontWeight: 'bold',
+                // textAlign: 'center',
+              }}
+              offsetY={5}
+              // closeOnDocumentClick={false}
+            >
+              <div className='popup-icons'>
+                <ul className='footer-icons'>
+                  <li className='share-icons'>
+                    <motion.a
+                      className='whatsapp'
+                      href='https://api.whatsapp.com/send?text=https://gabey.me/resume'
+                      data-action='share/whatsapp/share'
+                      style={{ color: 'white', fontSize: '1.2rem' }}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      whileHover={{ scale: 1.2 }}
+                      whileTap={{ scale: 0.9, x: '-5px', y: '5px' }}
+                    >
+                      <AiOutlineWhatsApp />
+                    </motion.a>
+                  </li>
+                  <li className='share-icons'>
+                    <motion.button
+                      onClick={() => onClickHandler()}
+                      style={{
+                        color: 'white',
+                        fontSize: '1.2rem',
+                        background: 'transparent',
+                        border: 'none',
+                      }}
+                      whileHover={{ scale: 1.2 }}
+                      whileTap={{ scale: 0.9, x: '-5px', y: '5px' }}
+                    >
+                      <AiFillCopy />
+                    </motion.button>
+                  </li>
+                </ul>
+              </div>
+            </Popup>
+          </Row>
+        </Zoom>
       </Container>
     </motion.div>
   )
